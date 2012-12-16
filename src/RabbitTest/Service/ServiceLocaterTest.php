3232e60ddeff61f785c973931a579aa8319f4c10
Erick Leao <erickleao@gmail.com>
2012-12-16 17:24:03 -0200

    Configurando phpunit

--- src/RabbitTest/Service/ServiceLocaterTest.php
+++ src/RabbitTest/Service/ServiceLocaterTest.php
@@ -1,1 +1,18 @@
 <?php
+namespace RabbitTest\Service;
+
+use Rabbit\Service\ServiceLocator;
+
+class ServiceLocaterTest extends \PHPUnit_Framework_TestCase {
+	
+	public function testRegisterService(){
+		
+		ServiceLocator::register("Rabbit\Service", function() {
+			return "servico";
+		});
+		
+		$this->assertTrue(ServiceLocator::isRegistred("Rabbit\Service"));
+		$this->assertEquals("servico", ServiceLocator::getService("Rabbit\Service"));
+	}
+	
+}
